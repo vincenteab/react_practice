56231d34f31a418d6df00d48aa29b2a7a105a0c4
vba16 <vba16@sfu.ca>
2023-09-13 20:56:33 -0700

    function to add items to list is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,44 @@
+import { useState } from "react"
+
 export default function App(){
+  //whatever is in the () of useState will be initialized to newItem
+  //setNewItem is going to update newItem acting like a "setter" in OOP 
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [...currentTodos,
+      {id: crypto.randomUUID(), title: newItem, completed: false},
+    ]
+    }) 
+  }
+console.log(todos)
   return(
     <>
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit} className="new-item-form">
     <div className="form-row"> 
       <label htmlFor="item">New Item</label>
-      <input type="text" id="item"/>
+      <input
+      value={newItem} 
+      onChange={e => setNewItem(e.target.value)} 
+      type="text" id="item"/>
     </div>
     <button className="btn">Add</button>
   </form>
   <h1 className="header">Todo List</h1>
   <ul className="list">
-    <li>
+    {todos.map(todo => {
+      return (
+      <li key={todo.id}>
       <label>
-        <input type="checkbox" />
-        Item 1
+        <input type="checkbox" checked={todo.completed}/>
+        {todo.title}
       </label>
       <button className="btn btn-danger">Delete</button>
-    </li>
-    <li>
-      <label>
-        <input type="checkbox" />
-        Item 2
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
+    </li>)
+    })}
   </ul>
   </> 
   )
